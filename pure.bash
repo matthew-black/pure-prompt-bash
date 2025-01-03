3907eda438cc2d8f295ba29c0061515557de6e36
Matthew Black <matthewallenblack@gmail.com>
2025-01-03 10:17:41 -0600

    open new pane/tab in current directory

--- pure.bash
+++ pure.bash
@@ -29,98 +29,51 @@ readonly RESET=$(tput sgr0)
 
 # symbols
 pure_prompt_symbol="❯"
-pure_symbol_unpulled="⇣"
-pure_symbol_unpushed="⇡"
 pure_symbol_dirty="*"
-pure_git_stash_symbol="≡"
 
 # if this value is true, remote status update will be async
 pure_git_async_update=false
 pure_git_raw_remote_status="+0 -0"
 
-
-__pure_echo_git_remote_status() {
-
-	# get unpulled & unpushed status
-	if ${pure_git_async_update}; then
-		# do async
-		# FIXME: this async execution doesn't change pure_git_raw_remote_status. so remote status never changes in async mode
-		# FIXME: async mode takes as long as sync mode
-		pure_git_raw_remote_status=$(git status --porcelain=2 --branch | grep --only-matching --perl-regexp '\+\d+ \-\d+') &
-	else
-		# do sync
-		pure_git_raw_remote_status=$(git status --porcelain=2 --branch | grep --only-matching --perl-regexp '\+\d+ \-\d+')
-	fi
-
-	# shape raw status and check unpulled commit
-	local readonly UNPULLED=$(echo ${pure_git_raw_remote_status} | grep --only-matching --perl-regexp '\-\d')
-	if [[ ${UNPULLED} != "-0" ]]; then
-		pure_git_unpulled=true
-	else
-		pure_git_unpulled=false
-	fi
-
-	# unpushed commit too
-	local readonly UNPUSHED=$(echo ${pure_git_raw_remote_status} | grep --only-matching --perl-regexp '\+\d')
-	if [[ ${UNPUSHED} != "+0" ]]; then
-		pure_git_unpushed=true
-	else
-		pure_git_unpushed=false
-	fi
-
-	# if unpulled -> ⇣
-	# if unpushed -> ⇡
-	# if both (branched from remote) -> ⇣⇡
-	if ${pure_git_unpulled}; then
-
-		if ${pure_git_unpushed}; then
-			echo "${RED}${pure_symbol_unpulled}${pure_symbol_unpushed}${RESET}"
-		else
-			echo "${BRIGHT_RED}${pure_symbol_unpulled}${RESET}"
-		fi
-
-	elif ${pure_git_unpushed}; then
-		echo "${BRIGHT_BLUE}${pure_symbol_unpushed}${RESET}"
-	fi
-}
-
 __pure_update_git_status() {
-
 	local git_status=""
 
-		# if current directory isn't git repository, skip this
-		if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == "true" ]]; then
+	# if current directory isn't git repository, skip this
+	if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == "true" ]]; then
+		git_status="$(git branch --show-current)"
 
-			git_status="$(git branch --show-current)"
+		# check clean/dirty
+		git_status="${git_status}$(git diff --quiet || echo "${pure_symbol_dirty}")"
 
-			# check clean/dirty
-			git_status="${git_status}$(git diff --quiet || echo "${pure_symbol_dirty}")"
-
-			# coloring
-			git_status="${BRIGHT_BLACK}${git_status}${RESET}"
-
-			# if repository have no remote, skip this
-			if [[ -n $(git remote show) ]]; then
-				git_status="${git_status} $(__pure_echo_git_remote_status)"
-			fi
-		fi
+		# coloring
+		git_status="${BRIGHT_BLACK}${git_status}${RESET}"
+	fi
 
 	pure_git_status=${git_status}
 }
 
 # if last command failed, change prompt color
 __pure_echo_prompt_color() {
-
 	if [[ $? = 0 ]]; then
 		echo ${pure_user_color}
 	else
 		echo ${RED}
 	fi
-
 }
 
 __pure_update_prompt_color() {
 	pure_prompt_color=$(__pure_echo_prompt_color)
+}
+
+# Cached directory tracking
+# https://claude.ai/chat/14a74d99-958a-47da-ba4a-a948decdac03
+__pure_track_directory() {
+    # Only update if directory has changed
+    if [[ "$PWD" != "$LAST_TRACKED_DIR" ]]; then
+        local win_path=$(cygpath -w "$PWD")
+        printf "\e]9;9;%s\e\\" "$win_path"
+        export LAST_TRACKED_DIR="$PWD"
+    fi
 }
 
 # if user is root, prompt is BRIGHT_YELLOW
@@ -129,17 +82,21 @@ case ${UID} in
 	*) pure_user_color=${YELLOW} ;;
 esac
 
+# Construct PROMPT_COMMAND
+PROMPT_COMMAND=()
+
 # if git isn't installed when shell launches, git integration isn't activated
 if [[ -n $(command -v git) ]]; then
-	PROMPT_COMMAND="__pure_update_git_status; ${PROMPT_COMMAND}"
+	PROMPT_COMMAND+=("__pure_update_git_status")
 fi
 
-PROMPT_COMMAND="__pure_update_prompt_color; ${PROMPT_COMMAND}"
+PROMPT_COMMAND+=("__pure_update_prompt_color")
+PROMPT_COMMAND+=("__pure_track_directory")
 
+# Convert PROMPT_COMMAND array to a string with semicolon separation
+PROMPT_COMMAND=$(IFS='; '; echo "${PROMPT_COMMAND[*]}")
 
 readonly FIRST_LINE="${MAGENTA}\w \${pure_git_status}\n"
-# raw using of $ANY_COLOR (or $(tput setaf ***)) here causes a creepy bug when go back history with up arrow key
-# I couldn't find why it occurs
 readonly SECOND_LINE="\[\${pure_prompt_color}\]${pure_prompt_symbol}\[$RESET\] "
 PS1="\n${FIRST_LINE}${SECOND_LINE}"
 
